7a5e2c6864dc86c78b4d2e8402523fd3fe3e8fd1
rickeygong <salabook@qq.com>
2022-08-03 22:48:58 +0800

    Fix update：数据类型

---
+++ 数据类型/数字类型.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+name = "rickey"
+age = 23 # 整型
+weight = 58.5 # 浮点型
+
+if __name__ == '__main__':
+    print(name)
+    print(age)
+    print(weight)
+    print(type(name))
+    print(type(age))
+    print(type(weight))
+
+
+
+
+
+
