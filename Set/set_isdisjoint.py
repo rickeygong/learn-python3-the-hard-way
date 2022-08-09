aee4ff45c16bd3b8d09541fbc352b66c01fd2709
rickeygong <salabook@qq.com>
2022-08-09 21:15:48 +0800

    提交

---
+++ Set/set_isdisjoint.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+a_set = {'name','xiaoming','xiaohong'}
+b_set = {'name','xiaogang','xiaolong'}
+c_set = {'name','xiaobao','xiaolin'}
+d_set = {'gongdh'}
+
+print(a_set.isdisjoint(b_set))
+print(a_set.isdisjoint(c_set))
+print(a_set.isdisjoint(d_set))
